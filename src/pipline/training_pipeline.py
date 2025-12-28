5c41cf69d3e5c99ba18cad7fa39369dca27bbc50
Rajesh Karel <rajesh.karel@gmail.com>
2025-12-29 01:32:30 +0530

    Till training estimation added

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -3,16 +3,16 @@ from src.exception import MyException
 from src.logger import logging
 
 from src.components.data_ingestion import DataIngestion
-# from src.components.data_validation import DataValidation
-# from src.components.data_transformation import DataTransformation
-# from src.components.model_trainer import ModelTrainer
+from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 # from src.components.model_evaluation import ModelEvaluation
 # from src.components.model_pusher import ModelPusher
 
-from src.entity.config_entity import (DataIngestionConfig
-                                        #   DataValidationConfig,
-                                        #   DataTransformationConfig,
-                                        #   ModelTrainerConfig,
+from src.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig,
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig
                                         #   ModelEvaluationConfig,
                                         #   ModelPusherConfig
                                           )
@@ -20,9 +20,9 @@ from src.entity.config_entity import (DataIngestionConfig
 from src.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
                                             DataTransformationArtifact,
-                                            ModelTrainerArtifact,
-                                            ModelEvaluationArtifact,
-                                            ModelPusherArtifact
+                                            ModelTrainerArtifact
+                                            # ModelEvaluationArtifact,
+                                            # ModelPusherArtifact
                                             )
 
 
@@ -30,9 +30,9 @@ from src.entity.artifact_entity import (DataIngestionArtifact,
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
-        # self.data_transformation_config = DataTransformationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
+        self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         # self.model_evaluation_config = ModelEvaluationConfig()
         # self.model_pusher_config = ModelPusherConfig()
 
@@ -100,32 +100,32 @@ class TrainPipeline:
         except Exception as e:
             raise MyException(e, sys)
 
-    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting modle evaluation
-        """
-        try:
-            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
-                                               data_ingestion_artifact=data_ingestion_artifact,
-                                               model_trainer_artifact=model_trainer_artifact)
-            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
-            return model_evaluation_artifact
-        except Exception as e:
-            raise MyException(e, sys)
+    # def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+    #                            model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+    #     """
+    #     This method of TrainPipeline class is responsible for starting modle evaluation
+    #     """
+    #     try:
+    #         model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+    #                                            data_ingestion_artifact=data_ingestion_artifact,
+    #                                            model_trainer_artifact=model_trainer_artifact)
+    #         model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+    #         return model_evaluation_artifact
+    #     except Exception as e:
+    #         raise MyException(e, sys)
 
-    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
-        """
-        This method of TrainPipeline class is responsible for starting model pushing
-        """
-        try:
-            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
-                                       model_pusher_config=self.model_pusher_config
-                                       )
-            model_pusher_artifact = model_pusher.initiate_model_pusher()
-            return model_pusher_artifact
-        except Exception as e:
-            raise MyException(e, sys)
+    # def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+    #     """
+    #     This method of TrainPipeline class is responsible for starting model pushing
+    #     """
+    #     try:
+    #         model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+    #                                    model_pusher_config=self.model_pusher_config
+    #                                    )
+    #         model_pusher_artifact = model_pusher.initiate_model_pusher()
+    #         return model_pusher_artifact
+    #     except Exception as e:
+    #         raise MyException(e, sys)
 
     def run_pipeline(self, ) -> None:
         """
@@ -133,10 +133,10 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            # data_transformation_artifact = self.start_data_transformation(
-            #     data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             # model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
             #                                                         model_trainer_artifact=model_trainer_artifact)
             # if not model_evaluation_artifact.is_model_accepted:
